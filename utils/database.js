522562250440c0e9f8bf52b5bd982ad7b006f651
Ayan-Alam <ayanalam186@gmail.com>
2023-10-31 10:32:59 +0530

    "storing hostname in env"

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,{
     dialect:'mysql',
-    host:'localhost'
+    host:process.env.DB_Host
 });
 
 module.exports = sequelize;
